afba50fbfc2c2d979620a658faf9a15b98ff4454
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2022-07-12 23:11:39 +0300

    Update SQLQueryUniversityScripts.sql

--- SQLQueryUniversityScripts.sql
+++ SQLQueryUniversityScripts.sql
@@ -8,34 +8,34 @@ insert into Department values
 ('Psychology', 105, 'Straight Street 3', '+7(555)5555555')
 
 insert into Professor values
-('professor1@gmail.com', 'Иванов Иван Иванович', 'French', 'Linguistics'),
-('professor2@gmail.com', 'Петров Петр Иванович', 'German', 'Linguistics'),
-('professor3@gmail.com', 'Иванов Сергей Владимирович', 'Ethics', 'Philosophy'),
-('professor4@gmail.com', 'Сидорова Мария Максимовна', 'Linear Algebra', 'Mathematics'),
-('professor5@gmail.com', 'Попов Алексей Эдуардович', 'Databases', 'Software Engineering'),
-('professor6@gmail.com', 'Кузнецова Марина Олеговна', 'Artificial Intelligence', 'Software Engineering'),
-('professor7@gmail.com', 'Васильев Александр Петрович', 'Clinical Psychology', 'Psychology')
+('professor1@gmail.com', 'РРІР°РЅРѕРІ РРІР°РЅ РРІР°РЅРѕРІРёС‡', 'French', 'Linguistics'),
+('professor2@gmail.com', 'РџРµС‚СЂРѕРІ РџРµС‚СЂ РРІР°РЅРѕРІРёС‡', 'German', 'Linguistics'),
+('professor3@gmail.com', 'РРІР°РЅРѕРІ РЎРµСЂРіРµР№ Р’Р»Р°РґРёРјРёСЂРѕРІРёС‡', 'Ethics', 'Philosophy'),
+('professor4@gmail.com', 'РЎРёРґРѕСЂРѕРІР° РњР°СЂРёСЏ РњР°РєСЃРёРјРѕРІРЅР°', 'Linear Algebra', 'Mathematics'),
+('professor5@gmail.com', 'РџРѕРїРѕРІ РђР»РµРєСЃРµР№ Р­РґСѓР°СЂРґРѕРІРёС‡', 'Databases', 'Software Engineering'),
+('professor6@gmail.com', 'РљСѓР·РЅРµС†РѕРІР° РњР°СЂРёРЅР° РћР»РµРіРѕРІРЅР°', 'Artificial Intelligence', 'Software Engineering'),
+('professor7@gmail.com', 'Р’Р°СЃРёР»СЊРµРІ РђР»РµРєСЃР°РЅРґСЂ РџРµС‚СЂРѕРІРёС‡', 'Clinical Psychology', 'Psychology')
 
 insert into StudentGroup values
-('19ПИ-1', 'group1@gmail.com', 'professor6@gmail.com'),
-('19ПИ-2', 'group2@gmail.com', 'professor6@gmail.com'),
-('19ФИПЛ-1', 'group3@gmail.com', 'professor1@gmail.com'),
-('19ФИПЛ-2', 'group4@gmail.com', 'professor2@gmail.com'),
-('19М', 'group5@gmail.com', 'professor4@gmail.com'),
-('18П', 'group6@gmail.com', 'professor7@gmail.com'),
-('21Ф', 'group7@gmail.com', 'professor3@gmail.com')
+('19РџР-1', 'group1@gmail.com', 'professor6@gmail.com'),
+('19РџР-2', 'group2@gmail.com', 'professor6@gmail.com'),
+('19Р¤РРџР›-1', 'group3@gmail.com', 'professor1@gmail.com'),
+('19Р¤РРџР›-2', 'group4@gmail.com', 'professor2@gmail.com'),
+('19Рњ', 'group5@gmail.com', 'professor4@gmail.com'),
+('18Рџ', 'group6@gmail.com', 'professor7@gmail.com'),
+('21Р¤', 'group7@gmail.com', 'professor3@gmail.com')
 
 insert into Student values
-('Козловский Максим Михайлович', 'student1@gmail.com', '19ПИ-1'),
-('Баженов Иван Станиславович', 'student2@gmail.com', '19ПИ-1'),
-('Новиков Матвей Ильич', 'student3@gmail.com', '19ПИ-2'),
-('Кузьмин Тимур Родионович', 'student4@gmail.com', '19ФИПЛ-1'),
-('Соколов Владимир Иванович', 'student5@gmail.com', '19ФИПЛ-2'),
-('Пономарева Алиса Андреевна', 'student6@gmail.com', '19М'),
-('Ефимова Николь Марковна', 'student7@gmail.com', '18П'),
-('Парамонова Виктория Данииловна', 'student8@gmail.com', '21Ф'),
-('Кузнецова Анна Ильинична', 'student9@gmail.com', '21Ф'),
-('Ковалева Амира Арсентьевна', 'student10@gmail.com', '19ПИ-2')
+('РљРѕР·Р»РѕРІСЃРєРёР№ РњР°РєСЃРёРј РњРёС…Р°Р№Р»РѕРІРёС‡', 'student1@gmail.com', '19РџР-1'),
+('Р‘Р°Р¶РµРЅРѕРІ РРІР°РЅ РЎС‚Р°РЅРёСЃР»Р°РІРѕРІРёС‡', 'student2@gmail.com', '19РџР-1'),
+('РќРѕРІРёРєРѕРІ РњР°С‚РІРµР№ РР»СЊРёС‡', 'student3@gmail.com', '19РџР-2'),
+('РљСѓР·СЊРјРёРЅ РўРёРјСѓСЂ Р РѕРґРёРѕРЅРѕРІРёС‡', 'student4@gmail.com', '19Р¤РРџР›-1'),
+('РЎРѕРєРѕР»РѕРІ Р’Р»Р°РґРёРјРёСЂ РРІР°РЅРѕРІРёС‡', 'student5@gmail.com', '19Р¤РРџР›-2'),
+('РџРѕРЅРѕРјР°СЂРµРІР° РђР»РёСЃР° РђРЅРґСЂРµРµРІРЅР°', 'student6@gmail.com', '19Рњ'),
+('Р•С„РёРјРѕРІР° РќРёРєРѕР»СЊ РњР°СЂРєРѕРІРЅР°', 'student7@gmail.com', '18Рџ'),
+('РџР°СЂР°РјРѕРЅРѕРІР° Р’РёРєС‚РѕСЂРёСЏ Р”Р°РЅРёРёР»РѕРІРЅР°', 'student8@gmail.com', '21Р¤'),
+('РљСѓР·РЅРµС†РѕРІР° РђРЅРЅР° РР»СЊРёРЅРёС‡РЅР°', 'student9@gmail.com', '21Р¤'),
+('РљРѕРІР°Р»РµРІР° РђРјРёСЂР° РђСЂСЃРµРЅС‚СЊРµРІРЅР°', 'student10@gmail.com', '19РџР-2')
 
 select * from Department
 select * from Professor
@@ -43,7 +43,7 @@ select * from StudentGroup
 select * from Student
 
 select * from Student where GroupId like '19%'
-select Name from Student where Name like '%Ко%'
+select Name from Student where Name like '%РљРѕ%'
 select top(1) Name, Addres from Department where Addres like 'Straight Street 1'
 select * from StudentGroup where Name like '19%' order by Name asc
 select distinct ProfessorEmail from StudentGroup
@@ -51,7 +51,7 @@ select distinct ProfessorEmail from StudentGroup
 update top(1) Department set Office = 101 where Addres like 'Straight Street 1'
 update top(1) Professor set Email = 'professor1@yandex.ru' where DepartmentName like 'Linguistics'
 
-delete top(1) from Professor where Name like 'Попов Алексей Эдуардович'
+delete top(1) from Professor where Name like 'РџРѕРїРѕРІ РђР»РµРєСЃРµР№ Р­РґСѓР°СЂРґРѕРІРёС‡'
 
 select ProfessorEmail, count(*) as Count from StudentGroup group by ProfessorEmail
 select Addres, count(*) as Count from Department group by Addres
